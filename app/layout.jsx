584affbc47f2baf6e9ad9f8e7cc6aa58aab37d58
xkamikoo <58092037+xkamikoo@users.noreply.github.com>
2023-06-19 14:19:00 +0200

    add navbar component with mobile and desktop navigations

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,4 @@
+import Navbar from '@components/Navbar'
 import '@styles/globals.css'
 
 export const metadata = {
@@ -14,6 +15,7 @@ const RootLayout = ({ children }) => {
             </div>
 
             <main className='app'>
+                <Navbar/>
                 {children}
             </main>
         </body>

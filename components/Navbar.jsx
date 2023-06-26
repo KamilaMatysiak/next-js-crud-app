c08f3a6a0052754be46da6eac1db6f22ce2e1056
xkamikoo <58092037+xkamikoo@users.noreply.github.com>
2023-06-26 15:31:40 +0200

    add sign in/out buttons based on session user

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,19 +6,19 @@ import { useState, useEffect } from 'react';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Navbar = () => {
-  const isUserLoggedIn = true;
+  const {data: session } = useSession();
 
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setupProviders = async () => {
         const responce = await getProviders();
 
         setProviders(responce);
     }
 
-    setProviders();
+    setupProviders();
   }, [])
   
 
@@ -30,7 +30,7 @@ const Navbar = () => {
         </Link>
 
         <div className='sm:flex hidden'>
-            {isUserLoggedIn 
+            {session?.user 
             ? <div className='flex gap-3 md:gap-5'>
                 <Link href="/create-prompt" className='black_btn'>
                     Create Post
@@ -62,7 +62,7 @@ const Navbar = () => {
 
         {/* Mobile navigation */}
         <div className='sm:hidden flex relative'>
-            {isUserLoggedIn
+            {session?.user 
             ? <div className='flex'>
                 <Image
                         src="/assets/images/logo.svg"
